8f43bb6149f95144592e29115a842d7e16513a94
Karen Atehortua <atehortuacastanokaren78@gmail.com>
2026-09-08 10:11:28 -0500

    refactor: centralize cors in gateway

--- customers/src/express-app.js
+++ customers/src/express-app.js
@@ -1,16 +1,12 @@
 const express = require('express');
-const cors = require('cors');
-const { customer } = require('./api');
-const HandleErrors = require('./utils/error-handler');
+const customerApi = require('./api');
+const { errorHandler } = require('./utils/error-handler');
 
 module.exports = async (app) => {
-    app.use(express.json({ limit: '1mb' }));
-    app.use(express.urlencoded({ extended: true, limit: '1mb' }));
-    app.use(cors());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
 
-    // API
-    customer(app);
+    customerApi(app);
 
-    // Error handling
-    app.use(HandleErrors);
+    app.use(errorHandler);
 };
--- shopping/src/express-app.js
+++ shopping/src/express-app.js
@@ -1,16 +1,12 @@
 const express = require('express');
-const cors = require('cors');
-const shopping = require('./api/shopping');
-const HandleErrors = require('./utils/error-handler');
+const shoppingApi = require('./api');
+const { errorHandler } = require('./utils/error-handler');
 
 module.exports = async (app) => {
-    app.use(express.json({ limit: '1mb' }));
-    app.use(express.urlencoded({ extended: true, limit: '1mb' }));
-    app.use(cors());
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
 
-    // API
-    shopping(app);
+    shoppingApi(app);
 
-    // Error handling
-    app.use(HandleErrors);
+    app.use(errorHandler);
 };
